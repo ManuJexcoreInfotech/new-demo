c6e9aed517a774ea0841d3986b3d9b741f03d3b6
Manu jexcore <web_dev_2@shubantecgnologies.com>
2017-10-02 13:30:47 +0530

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,27 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+		
         this.receivedEvent('deviceready');
+		window.plugins.AdMob.setOptions( {
+          publisherId: "ca-app-pub-7844737046957946/9216355879",
+          interstitialAdId: "ca-app-pub-7844737046957946/5278703180",
+          bannerAtTop: false, // set to true, to put banner at top
+          overlap: false, // set to true, to allow banner overlap webview
+          offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+          isTesting: false, // receiving test ad
+          autoShow: true // auto show interstitial ad when loaded
+        });
+        // display the banner at startup
+        window.plugins.AdMob.createBannerView();
+        
+        // create interstitial ad
+        window.plugins.AdMob.createInterstitialView();
+        window.plugins.AdMob.showInterstitialAd(
+          true, 
+          function(){},
+          function(e){alert(JSON.stringify(e));}
+        );
     },
 
     // Update DOM on a Received Event
